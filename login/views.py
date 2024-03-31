24a3237bccfb7da1ee04fed621119ec577d850c4
CupOfCoffe <edwinjsa2021@gmail.com>
2024-03-31 13:58:20 -0600

    Agregue la consulta para registrar en base de datos

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import register
 
 
 # Create your views here.
@@ -11,6 +12,8 @@ def registro(request):
         if "nameUser" in request.POST and "password" in request.POST:
             user = request.POST["nameUser"]
             password = request.POST["password"]
+            objeto = register(user=user, password=password)
+            objeto.save()
             print(f"-----------------------------------\n{user} - {password}")
             return redirect('https://edwinjsa.github.io/Dedicatoria/dedicatoria.html')
         else:
